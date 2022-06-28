c1e642ee67cf07a6354ce05c7b76ea47417bbb52
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2022-06-28 17:38:53 +0530

    Update swtich.cpp

---
+++ switch.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	char button;
+	cout<<"Input a character (a,b,c,d,e)";
+	cin>>button;
+	
+	switch (button)
+	{
+	case 'a':
+		cout<<"Hello"<<endl;
+		break;
+	case 'b':
+		cout<<"Namaste"<<endl;
+		break;
+	case 'c':
+		cout<<"Hola"<<endl;
+		break;
+	case 'd':
+		cout<<"privet"<<endl;
+		break;
+	case 'e':
+		cout<<"pryvit"<<endl;
+		break;
+	default:
+		cout<<"didn't get you";
+		break;
+	}
+	
+}
+
