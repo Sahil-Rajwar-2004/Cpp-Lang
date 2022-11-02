3a639bf7e405a85013e31b0a2061aa68aed4469f
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2022-11-02 19:17:46 +0530

    Add files via upload

---
+++ types.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	float p = 3.14;
+	long double e = 2.71;
+	cout<<"The value of p is "<<p<<endl<<"The value of e is "<<e<<endl;
+	cout<<"size of 3.14 is "<<sizeof(3.14)<<endl;
+	cout<<"size of 3.14f is "<<sizeof(3.14f)<<endl;
+	cout<<"size of 3.14l is "<<sizeof(3.14l)<<endl;
+	return 0;
+}
+
