8b6ed3064e87a6fc3da69c5be8e6796912b1bdd9
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2022-11-08 19:30:30 +0530

    Add files via upload

---
+++ square_1-4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	int i;
+	cin>>n;
+	while(i<=n){
+		int j = 1;
+		while(j<=n){
+			cout<<j;
+			j++;
+		}
+		cout<<endl;
+		i++;
+	}
+	return 0;
+}
+
