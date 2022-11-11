0152e31465e114caebeffa082738d0b4e7150a96
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2022-11-11 21:16:01 +0530

    Add files via upload

---
+++ pattern5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    int i=1;
+    int count=1;
+    cin>>n;
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            cout<<count;
+            count++;
+            j++;
+        }
+        cout<<endl;
+        i++;
+        
+    }
+    return 0;
+}
