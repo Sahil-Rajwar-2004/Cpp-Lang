363ead7694c4ac3ac0ba55ffec06747216816e87
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2022-11-28 18:52:08 +0530

    Add files via upload

---
+++ abcbcdcde.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	int i=1;
+	while(i<=n){
+		int j=1;
+		while(j<=n){
+			char chr = 'A'+i+j-2;
+			cout<<chr;
+			j++;
+		}
+		cout<<endl;
+		i++;
+	}
+	return 0;
+}
+
