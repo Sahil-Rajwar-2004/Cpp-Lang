bd34f7215afbacc99b01dcf321bd5e05663d3972
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2022-12-02 22:48:25 +0530

    Update and rename reverseint.cpp to reverse_integer.cpp

--- reverse_integer.cpp
+++ reverse_integer.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 
 int main(){
@@ -7,6 +8,9 @@ int main(){
 	int ans = 0;
 	while(n!=0){
 		int digit = n%10;
+		if((ans>INT_MAX/10)||(ans<INT_MIN/10)){
+			return 0;
+		}
 		ans = ans*10+digit;
 		n = n/10;
 	}
