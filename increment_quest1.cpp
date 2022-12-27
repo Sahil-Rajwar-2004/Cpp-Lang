277f55c3bfc7e43ad6153e4a5687af4f5c5d6233
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2022-12-27 18:57:12 +0530

    Add files via upload

---
+++ increment_quest1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a,c;
+	cin>>a;
+	c = ++a + ++a;
+	cout<<c<<endl;
+	return 0;
+}
+
---
+++ increment_quest4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	n = n++ - --n + ++n - n--;
+	cout<<n<<endl;
+	return 0;
+}
+
