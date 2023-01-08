2bce57ad888e92e5638b099ba1a7afb9ff822513
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2023-01-08 18:05:21 +0530

    Add files via upload

---
+++ binary_floyd_triangle.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n,start = 0;
+	cin>>n;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<=i;j++){
+			if(start == 0){
+				start = 1;
+			}else{
+				start = 0;
+			}
+			cout<<start;
+		}
+		cout<<endl;
+	}
+
+	return 0;
+}
+
+
